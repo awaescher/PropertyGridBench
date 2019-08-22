d1331bf4e02e6283fbc4c7b6f39031e39065e753
Andreas Wäscher <awaescher@sodacore.net>
2019-08-22 23:30:34 +0200

    Add tons of binaries

---
+++ 1913/TestForm.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1913
+{
+	public partial class TestForm : XtraForm
+	{
+		public TestForm()
+		{
+			InitializeComponent();
+		}
+
+		private void PropertyGridControl1_CustomPropertyDescriptors(object sender, DevExpress.XtraVerticalGrid.Events.CustomPropertyDescriptorsEventArgs e)
+		{
+			System.Threading.Thread.Sleep(1000);
+		}
+
+		private void ButtonLoad_Click(object sender, EventArgs e)
+		{
+			var sw = Stopwatch.StartNew();
+			propertyGridControl1.SelectedObject = this;
+			label1.Text = $"Took {sw.ElapsedMilliseconds / 1000} seconds.";
+		}
+	}
+}
